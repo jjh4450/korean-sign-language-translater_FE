88d72db44381951370216506d33ec0b8fb1ed3e5
JunHWan Jo (jeje) <jjh4450git@gamil.com>
2024-06-04 01:11:32 +0900

    기본 구성 완료

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Pretendard Variable"', 'Pretendard', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
