35dc0f42a5ce96cc52c40538e4c70d7fccb4d201
JunHWan Jo (jeje) <jjh4450git@gamil.com>
2024-06-20 01:21:29 +0900

    title 요소 추가를 통해 접근성 향상

--- src/components/ServerResponse.jsx
+++ src/components/ServerResponse.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+/**
+ * ServerResponse 컴포넌트는 서버 응답을 표시하는 역할을 합니다.
+ * @param {Object} props - 컴포넌트에 전달되는 props 객체
+ * @param {Object} props.response - 서버 응답 객체
+ * @returns {JSX.Element} 서버 응답을 표시하는 컴포넌트
+ */
+// ServerResponse 컴포넌트는 서버 응답을 표시하는 역할을 합니다.
+// response prop을 받아와서 JSON 형식으로 보여줍니다.
 const ServerResponse = ({ response }) => (
   <div className="mt-8 bg-gray-100 p-4 rounded-lg shadow-inner">
     <h2 className="text-lg font-medium text-gray-800">서버 응답:</h2>
--- src/pages/Showcase/ControlPanel.jsx
+++ src/pages/Showcase/ControlPanel.jsx
@@ -39,6 +39,7 @@ const ControlPanel = () => {
         className={`shadow-lg w-1/2 h-1/2 inline-flex items-center justify-center text-white border-0 py-2 px-4 focus:outline-none text-lg rounded-bl-lg ${isLooping ? 'hover:bg-gray-200 bg-gray-100' : 'hover:bg-indigo-600 bg-indigo-500'}`}
         onClick={toggleLooping}
         aria-label="Toggle Looping"
+        title={isLooping ? '반복 재생 비활성화' : '반복 재생 활성화'}
       >
         {isLooping ? '🔁' : '⏭️'}
       </button>
@@ -46,6 +47,7 @@ const ControlPanel = () => {
         className="shadow-lg w-1/2 inline-flex items-center justify-center text-white bg-gray-100 border-0 py-2 px-4 focus:outline-none rounded-br-lg text-lg hover:bg-gray-200 bg-gray-100"
         onClick={handlePip}
         aria-label="Toggle Picture in Picture"
+        title="영상을 상단에 고정하기"
       >
         <Pipicon />
       </button>
